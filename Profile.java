f1c143d6979004a81ce07d9d3efb219ee9af292c
sriharshave <37176709+sriharshave@users.noreply.github.com>
2018-04-02 00:48:44 +1000

    Add files via upload

---
+++ Profile.java
@@ -0,0 +1,36 @@
+package example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Profile {
+	//author harsha
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    List<Friend> mFriendList = new ArrayList<>();
+    String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Friend> getmFriendList() {
+        return mFriendList;
+    }
+
+    public void setmFriendList(List<Friend> mFriendList) {
+        this.mFriendList = mFriendList;
+    }
+}
